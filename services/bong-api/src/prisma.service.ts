4cfee0fb3cc1d6fd190efe51e14f86c7921405b3
Kebert <rasmus.kebert@gmail.com>
2026-06-22 13:08:18 +0200

    Trying to fix the test

--- services/bong-api/src/prisma.service.ts
+++ services/bong-api/src/prisma.service.ts
@@ -1,7 +1,7 @@
-import "dotenv/config";
-import { Injectable } from "@nestjs/common";
-import { PrismaClient } from "./generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import 'dotenv/config';
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from './generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
